a194d229c9999b09e06ba08dd5bf0a7dd3e20923
jiyeonahn <ajy7424@naver.com>
2024-08-07 14:37:18 +0900

    reponse header에 Server-Post 추가

---
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AddResponseHeaderWebFilter.java
@@ -0,0 +1,23 @@
+package com.sparta.msa_exam.auth;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AddResponseHeaderWebFilter implements Filter {
+
+    @Value("${server.port}")
+    String port;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        httpServletResponse.setHeader(
+                "Server-Port", port);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
